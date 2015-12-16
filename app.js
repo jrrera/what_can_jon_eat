3e124d4dd9e364a4eb0c95da58958d47cd3450c2
jrrera <jrrera@gmail.com>
2015-12-16 08:36:29 -0800

    Basic HTML template with tests

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
+
 app.get('/', function(req, res) {
   res.send('OK')
+});
+
+app.get('/foods', function(req, res) {
+  var foods = ['apples', 'pears', 'jalapenos'];
+  res.json(foods);
 });
 
 module.exports = app;
--- tests.js
+++ tests.js
@@ -7,13 +7,40 @@ describe('Requests to the root path', function() {
 
     request(app)
       .get('/')
-      .expect(200)
-      .end(function(error) {
-        if (error) throw error;
-        done();
-      });
+      .expect(200, done);
 
   });
 
+  it('Returns an HTML format', function(done) {
+    request(app)
+      .get('/')
+      .expect('Content-Type', /html/, done);
+  });
 
+  it('Returns an index file with Foods', function(done) {
+     request(app)
+       .get('/')
+       .expect(/foods/i, done);
+  });
 });
+
+describe('Listing foods I can eat on /foods', function() {
+  it('returns 200 status code', function(done) {
+    request(app)
+      .get('/foods')
+      .expect(200, done);
+  });
+
+  it('returns JSON format', function(done) {
+    request(app)
+      .get('/foods')
+      .expect('Content-Type', /json/, done)
+  });
+
+  it('returns initial foods', function(done) {
+     request(app)
+      .get('/foods')
+      .expect(JSON.stringify(['apples', 'pears', 'jalapenos']), done);
+  });
+});
+
