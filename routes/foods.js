f9a5a267a874964e84e2a2c6fea685fb13d88ba2
jrrera <jrrera@gmail.com>
2015-12-17 17:42:08 -0800

    refactors redis client into lib module

--- routes/foods.js
+++ routes/foods.js
@@ -2,26 +2,12 @@ var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
 
-// Redis
-var redis = require('redis');
-var client, rtg;
-
-if (process.env.REDISTOGO_URL) {
-  rtg = require("url").parse(process.env.REDISTOGO_URL);
-  client = redis.createClient(rtg.port, rtg.hostname);
-  client.auth(rtg.auth.split(":")[1]);
-} else {
-  client = redis.createClient();
-  // 'dev' vs 'test' vs 'production' all have different
-  // integer values for using different databases.
-  client.select((process.env.NODE_ENV || 'development').length);
-}
-// end Redis
+var redisClient = require('../lib/redis');
 
 
 router.route('/')
   .get(function(req, res) {
-    client.hgetall('foods', function(error, foodObjects) {
+    redisClient.hgetall('foods', function(error, foodObjects) {
       if (error) throw error;
       // Coerce string values to boolean.
       for (var food in foodObjects) {
@@ -38,19 +24,19 @@ router.route('/')
       res.sendStatus(400);
       return;
     }
-    client.hset('foods', newFood.name, newFood.eat);
+    redisClient.hset('foods', newFood.name, newFood.eat);
     res.status(201).json(newFood.name);
     });
 
 router.route('/:name')
   .delete(function(req, res) {
-    client.hdel('foods', req.params.name, function(error) {
+    redisClient.hdel('foods', req.params.name, function(error) {
       if (error) throw error;
       res.sendStatus(204);
     });
   })
   .get(function(req, res) {
-    client.hget('foods', req.params.name, function(error, description) {
+    redisClient.hget('foods', req.params.name, function(error, description) {
       res.json(description);
     });
   });
